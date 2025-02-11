ac1bbeb5dc4407636850895a58faa3021e38c52a
AMDADUS SOBHAN <amdadulislamcl@gmail.com>
2025-02-11 23:51:28 +0600

    Added Booking List in Home View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Room_Booking_System.Data;
 using Room_Booking_System.Models;
 
 namespace Room_Booking_System.Controllers;
 
 public class HomeController : Controller
 {
+    AppDbContext _db;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
+        _db = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
+        ViewBag.bookings = _db.Bookings;
         return View();
     }
 }
